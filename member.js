1f94c7f2d6c5d59895d9127639a5e5ef9db6de5f
softwaredoctr <67086445+softwaredoctr@users.noreply.github.com>
2024-06-05 20:20:53 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,7 @@
+function skillsMember() {
+    var member = new Array();
+    member["name"] = "John Doe";
+    member["age"] = 26;
+    member["skills"] = ["C#", "JavaScript", "HTML", "CSS"];
+    return member;
+}
